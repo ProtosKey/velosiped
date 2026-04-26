e0330442dbf38b66127a48996b372b8d2b10b7da
Rinat <rinat.gazizullin.31@bk.ru>
2026-04-26 13:44:44 +0300

    feat: added output writer

---
+++ src/utils/console_writer.c
@@ -0,0 +1,22 @@
+#include "utils/vls_writer.h"
+#include <string.h>
+#include <unistd.h>
+
+int vls_write_all(vls_output_t out) {
+  size_t count = 0;
+  ssize_t n;
+
+  if (out.size == 0) {
+    return 0;
+  }
+
+  while (count < out.size) {
+    n = write(out.descriptor, out.message + count, out.size - count);
+    if (n == -1 || n == 0) {
+      return -1;
+    }
+    count += n;
+  }
+
+  return 0;
+}
