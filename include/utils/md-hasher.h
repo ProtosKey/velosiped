ff26d4fa5cbe88574f865fbe490723e412e9e19b
Rinat <rinat.gazizullin.31@bk.ru>
2026-04-26 18:24:56 +0300

    feat: added hasher realization

--- include/utils/md-hasher.h
+++ include/utils/md-hasher.h
@@ -1,11 +1,22 @@
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 4096
+#endif
+
+#ifndef MD_SIZE
+#define MD_SIZE 16
+#endif
+
 #ifndef MD_HESHER_H
 #define MD_HESHER_H
+
+#include <openssl/md5.h>
 
 typedef struct {
   unsigned char bytes[16];
 } vls_md_hash;
 
-int hash_my_path(const char *path, vls_md_hash *result);
-int vls_hash_to_string(const vls_md_hash *hash, char *result);
+int hash_my_path(const char *, vls_md_hash *);
+int vls_hash_to_string(const vls_md_hash *, char *);
+int is_identical(const vls_md_hash *, const vls_md_hash *);
 
 #endif
---
+++ src/utils/vls_hasher.c
@@ -0,0 +1,91 @@
+#include "utils/md-hasher.h"
+#include "utils/vls_writer.h"
+#include <fcntl.h>
+#include <openssl/evp.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int hash_my_path(const char *path, vls_md_hash *result) {
+  int fd = 0;
+  const char *no_sush_file = "No such file";
+  if ((fd = open(path, O_RDONLY)) < 0) {
+    vls_safety_write(
+        (vls_output_t){STDERR_FILENO, no_sush_file, strlen(no_sush_file)});
+    return -1;
+  }
+
+  struct stat file;
+  if (stat(path, &file) < 0) {
+    vls_safety_write(
+        (vls_output_t){STDERR_FILENO, no_sush_file, strlen(no_sush_file)});
+    return -1;
+  }
+
+  const char *not_a_file = "Not a file";
+  if (!S_ISREG(file.st_mode)) {
+    vls_safety_write(
+        (vls_output_t){STDERR_FILENO, not_a_file, strlen(not_a_file)});
+    return -1;
+  }
+
+  const char *not_md = "Cannot create hash";
+  EVP_MD_CTX *contex = EVP_MD_CTX_new();
+  if (contex == NULL) {
+    goto hash_error;
+  }
+
+  if (EVP_DigestInit_ex(contex, EVP_md5(), NULL) != 1) {
+    goto hash_error;
+  }
+
+  unsigned char buff[BUFFER_SIZE];
+  ssize_t bytes_read;
+
+  while (true) {
+    bytes_read = read(fd, buff, BUFFER_SIZE);
+    if (bytes_read < 0) {
+      goto hash_error;
+    } else if (bytes_read == 0) {
+      break;
+    }
+    if (EVP_DigestUpdate(contex, buff, bytes_read) != 1) {
+      goto hash_error;
+    }
+  }
+
+  if (bytes_read < 0) {
+    goto hash_error;
+  }
+
+  unsigned char md_buff[EVP_MAX_MD_SIZE];
+  unsigned int len;
+  EVP_DigestFinal_ex(contex, md_buff, &len);
+  EVP_MD_CTX_free(contex);
+  close(fd);
+
+  memcpy(result->bytes, md_buff, MD_SIZE);
+  return 0;
+hash_error:
+  EVP_MD_CTX_free(contex);
+  vls_safety_write((vls_output_t){STDERR_FILENO, not_md, strlen(not_md)});
+  close(fd);
+  return -1;
+}
+
+int vls_hash_to_string(const vls_md_hash *hash, char *result) {
+  static const char *hex = "0123456789abcdef";
+
+  for (int i = 0; i < MD_SIZE; i++) {
+    result[i * 2] = hex[(hash->bytes[i] >> 4) & 0x0F];
+    result[i * 2 + 1] = hex[hash->bytes[i] & 0x0F];
+  }
+
+  return 0;
+}
+
+int is_identical(const vls_md_hash *first, const vls_md_hash *second) {
+  return memcmp(first->bytes, second->bytes, MD_SIZE);
+}
