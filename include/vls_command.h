d6e4c1f626cadc6ef9c829a7dc63abcf1e6c198d
Prokhor <loskutovp333@gmail.com>
2026-04-26 16:19:54 +0300

    refactor: edited signature of command type

--- include/vls_command.h
+++ include/vls_command.h
@@ -4,13 +4,18 @@
 typedef struct {
   const char *command_name;
   const char *description;
-  const char **argv;
-  const int argc;
-} command_data_t;
+  int (*ptr)(const int c, const char **argv);
+} command_t;
 
-int vls_init(const command_data_t command_data);
-int vls_add(const command_data_t command_data);
-int vls_commit(const command_data_t command_data);
-int vls_reset(const command_data_t command_data);
+int vls_init_func(const int c, const char **argv);
+command_t vls_init = {"init", "-", vls_init_func};
+int vls_add_func(const int c, const char **argv);
+command_t vls_add = {"init", "-", vls_add_func};
+int vls_commit_func(const int c, const char **argv);
+command_t vls_commit = {"init", "-", vls_commit_func};
+int vls_reset_func(const int c, const char **argv);
+command_t vls_reset = {"init", "-", vls_reset_func};
+int vls_help_func(const int c, const char **argv);
+command_t vls_help = {"init", "-", vls_help_func};
 
 #endif
