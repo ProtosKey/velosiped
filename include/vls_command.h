c2b9b4366224c7590e631056658d8e486e9c6a32
Prokhor <loskutovp333@gmail.com>
2026-04-26 16:30:10 +0300

    style: edited naming of shadow varibles

--- include/vls_command.h
+++ include/vls_command.h
@@ -4,18 +4,19 @@
 typedef struct {
   const char *command_name;
   const char *description;
-  int (*ptr)(const int c, const char **argv);
+  int (*ptr)(const int, const char **);
 } command_t;
 
-int vls_init_func(const int c, const char **argv);
+int vls_init_func(const int, const char **);
+int vls_add_func(const int, const char **);
+int vls_commit_func(const int, const char **);
+int vls_reset_func(const int, const char **);
+int vls_help_func(const int, const char **);
+
 command_t vls_init = {"init", "-", vls_init_func};
-int vls_add_func(const int c, const char **argv);
 command_t vls_add = {"init", "-", vls_add_func};
-int vls_commit_func(const int c, const char **argv);
 command_t vls_commit = {"init", "-", vls_commit_func};
-int vls_reset_func(const int c, const char **argv);
 command_t vls_reset = {"init", "-", vls_reset_func};
-int vls_help_func(const int c, const char **argv);
 command_t vls_help = {"init", "-", vls_help_func};
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,9 +1,3 @@
 #include <unistd.h>
 
-int main() {
-  ssize_t n = write(1, "Hello, World!\n", 14);
-  if (n < 0) {
-    return 1;
-  }
-  return 0;
-}
+int main() {}
