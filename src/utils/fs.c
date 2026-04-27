f43f2368ceb4ea13849e43958d7a569577e170df
Prokhor <loskutovp333@gmail.com>
2026-04-27 19:19:06 +0300

    fix: vargs in vls_ensure_file in fs header

--- src/utils/fs.c
+++ src/utils/fs.c
@@ -2,24 +2,36 @@
 #include "utils/logger.h"
 #include <errno.h>
 #include <fcntl.h>
+#include <stdarg.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/cdefs.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-int vls_ensure_dir(const char *path) {
+__attribute__((nonnull(1), warn_unused_result())) int
+vls_ensure_dir(const char *path) {
   if (mkdir(path, 0755) < 0 && errno != EEXIST) {
     return vls_report_errno_at(path, errno);
   }
   return 0;
 }
 
-int vls_ensure_file(const char *path, int oflag, ...) {
+__attribute__((nonnull(1), warn_unused_result(), const)) int
+vls_ensure_file(const char *path, int oflag, ...) {
+  int mod = 0;
+  if (oflag & O_CREAT) {
+    va_list list;
+    va_start(list, oflag);
+    mod = va_arg(list, int);
+    va_end(list);
+  }
+
   int fd = -1;
-  if ((fd = open(path, oflag)) < 0) {
+  if ((fd = open(path, oflag, mod)) < 0) {
     return vls_report_errno_at(path, errno);
   }
   return fd;
