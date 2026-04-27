8bc5e45aa09384b7358dae166fd0e5ad231c0875
Rinat <rinat.gazizullin.31@bk.ru>
2026-04-27 21:38:47 +0300

    feat: added help command, added easy descriptions

--- src/commands/help.c
+++ src/commands/help.c
@@ -1,2 +1,43 @@
+#include "utils/logger.h"
+#include "vls_command.h"
 
-int vls_help_func(const int argc, const char **argv) { return 0; }
+#define CLR_RESET "\033[0m"
+#define CLR_BOLD "\033[1m"
+#define CLR_CYAN "\033[36m"
+#define CLR_GREEN "\033[32m"
+#define CLR_WHITE "\033[37m"
+#define CLR_GRAY "\033[90m"
+
+#define COMMANDS(X)                                                            \
+  X(init)                                                                      \
+  X(help)                                                                      \
+  X(add)                                                                       \
+  X(commit)
+
+#define PRINT_HELP(name)                                                       \
+  vls_quick("\n");                                                             \
+  vls_quick(vls_##name.command_name);                                          \
+  vls_quick("\t—\t");                                                          \
+  vls_quick(vls_##name.description);                                           \
+  vls_quick("\n");
+
+int vls_help_func(const int argc, const char **argv) {
+  vls_quick("\n");
+  vls_say("VeLosiped System (vls) - version control system.");
+
+  vls_say(CLR_BOLD CLR_GREEN);
+  vls_say("      (`-.                .-')    ");
+  vls_say("    _(OO  )_             ( OO ).  ");
+  vls_say(",--(_/   ,. \\ ,--.      (_)---\\_) ");
+  vls_say("\\   \\   /(__/ |  |.-')  /    _ |  ");
+  vls_say(" \\   \\ /   /  |  | OO ) \\  :` `.  ");
+  vls_say("  \\   '   /,  |  |`-' |  '..`''.) ");
+  vls_say("   \\     /__)(|  '---.' .-._)   \\ ");
+  vls_say("    \\   /     |      |  \\       / ");
+  vls_say("     `-'      `------'   `-----'  ");
+  vls_say(CLR_RESET);
+
+  COMMANDS(PRINT_HELP);
+
+  return 0;
+}
--- src/vls_command.c
+++ src/vls_command.c
@@ -1,7 +1,19 @@
 #include "vls_command.h"
 
-command_t vls_init = {"init", "-", vls_init_func};
-command_t vls_add = {"add", "-", vls_add_func};
-command_t vls_commit = {"commit", "-", vls_commit_func};
-command_t vls_reset = {"reset", "-", vls_reset_func};
-command_t vls_help = {"help", "-", vls_help_func};
+command_t vls_init = {
+    "init", "Initialize the lair (.md) and start the clock of history",
+    vls_init_func};
+
+command_t vls_add = {"add", "Stage files or directories for the next snapshot",
+                     vls_add_func};
+
+command_t vls_commit = {
+    "commit", "Seal the current stage into an immortal history record",
+    vls_commit_func};
+
+command_t vls_reset = {"reset",
+                       "Turn back time and roll back to the last clean state",
+                       vls_reset_func};
+
+command_t vls_help = {
+    "help", "Show this manual for survivors and code-warriors", vls_help_func};
