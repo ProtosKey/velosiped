eac98b69d3d88a00b830d5644a5d8a8590bff299
Rinat <rinat.gazizullin.31@bk.ru>
2026-04-29 06:31:48 +0300

    fix: style in hasher header fixed

--- include/utils/hasher.h
+++ include/utils/hasher.h
@@ -7,8 +7,7 @@
 #include <stddef.h>
 
 int hash_my_path(const char *, vls_md_hash_t *);
-int hash_my_path_and_bytes(const char *, const void *, size_t,
-                           vls_md_hash_t *);
+int hash_my_path_and_bytes(const char *, const void *, size_t, vls_md_hash_t *);
 int hash_to_string(const vls_md_hash_t *, char *);
 int hash_from_string(char *, vls_md_hash_t *);
 int is_identical(const vls_md_hash_t *, const vls_md_hash_t *);
